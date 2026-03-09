6dd41e3212fede7bb592c7a69271fc3b96e0dd69
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-10 01:20:35 +0600

    Create Payment Invoice PDF Generation And Sending Payment Invoice Email

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,6 +8,8 @@ import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
 import { AppointmentRoutes } from "../module/appointment/appointment.route";
 import { PatientRoutes } from "../module/patient/patient.route";
+import { PrescriptionRoutes } from "../module/prescription/prescription.route";
+import { ReviewRoutes } from "../module/review/review.route";
 
 const router = Router();
 
@@ -20,5 +22,7 @@ router.use("/admins", AdminRoutes);
 router.use("/schedules", scheduleRoutes);
 router.use("/doctor-schedules", DoctorScheduleRoutes);
 router.use("/appointments", AppointmentRoutes);
+router.use("/prescriptions", PrescriptionRoutes);
+router.use("/reviews", ReviewRoutes);
 
 export const IndexRoutes = router;
